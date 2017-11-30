53a05e5be2dcfbea3b08b2f5f91348f089b2d1e4
dcaust1n <31997659+dcaust1n@users.noreply.github.com>
2017-11-30 15:12:16 -0500

    Create space.cpp
    jailspace and freespace functions defined

---
+++ space.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "space.h"
+
+class jail{
+    public:
+    //this needs to be virtual to be overridden in derived class
+    virtual void jailspace(){}
+    //this is overloaded function of the previous one
+    void show(int x){std::cout<<"\nI am overloaded";} 
+};
+
+class derived:public free{
+    do_action;
+};
+
+
+int main(){
+    if (in_jail==true){
+      
+    
+    }
+    
+    
+}
